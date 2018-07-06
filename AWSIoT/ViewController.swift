47359f064d6a348ec2b6df5ba3d65cefde74a5b6
Takayamashi <ryu1208.takayama@gmail.com>
2018-07-06 20:09:38 +0900

    Initial Commit

---
+++ AWSIoT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWSIoT
+//
+//  Created by Ryuichi Takayama on 2018/07/06.
+//  Copyright © 2018年 takayamashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
